641b7814050cd6ecb87c8c292bd318334908d037
Gu0Yu <guoyu0106@163.com>
2018-06-12 03:49:52 +0000

    hh

--- config/services.php
+++ config/services.php
@@ -30,7 +30,7 @@ return [
     ],
 
     'stripe' => [
-        'model' => App\User\Models::class,
+        'model' => App\Models\User\Models::class,
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
